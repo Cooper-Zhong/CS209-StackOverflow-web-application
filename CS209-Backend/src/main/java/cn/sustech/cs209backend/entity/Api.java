e57ed731399e5248abe2ee00bef86fd6b206548e
Cooper <12110517@mail.sustech.edu.cn>
2023-12-09 22:48:24 +0800

    modify column name. Cancel foreign key on post_id of comment.

--- CS209-Backend/src/main/java/cn/sustech/cs209backend/entity/Api.java
+++ CS209-Backend/src/main/java/cn/sustech/cs209backend/entity/Api.java
@@ -2,6 +2,7 @@ package cn.sustech.cs209backend.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name="apis")
 public class Api {
 
     @Id
--- CS209-Backend/src/main/java/cn/sustech/cs209backend/entity/Tag.java
+++ CS209-Backend/src/main/java/cn/sustech/cs209backend/entity/Tag.java
@@ -3,6 +3,7 @@ package cn.sustech.cs209backend.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name="tags")
 public class Tag {
 
     @Id
