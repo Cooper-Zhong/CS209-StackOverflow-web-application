04581164b74409a3f5a6ce2465ee88f66c3fbff9
ultramarine <2549952274@qq.com>
2023-12-11 17:41:02 +0800

    主页+bar图框架（只接了接口 暂时没有测试 还没有把数据体现出来）

---
+++ frontend/src/components/topic/topicQuestionBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+  />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+import {ref, defineComponent, onMounted, getCurrentInstance} from 'vue';
+import axios from "axios";
+import {useToast} from "vuestic-ui";
+export default defineComponent({
+  name: 'BarChart',
+  components: { Bar },
+  setup(){
+    const appConfig = ref(getCurrentInstance().appContext.config.globalProperties).value;
+    axios.defaults.baseURL = appConfig.$apiBaseUrl;
+    const {init} = useToast();
+    const items = ref([]);
+    const getTopicsByAnswers = () => {
+      axios.post('/topKByQuestionCount/10', {}, {})
+          .then(response => {
+            items.value = response.data.data
+          })
+          .catch(error => {
+            if (error.response) {
+              init({message: error.response.data.msg, color: "danger"})
+            } else {
+              init({message: error.message, color: "danger"})
+
+            }
+          });
+    };
+    onMounted(() => {
+      getTopicsByAnswers();
+    });
+    return{
+      items,
+    };
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [ 'January', 'February', 'March' ],
+        datasets: [ { data: [40, 20, 12] } ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+})
+</script>
