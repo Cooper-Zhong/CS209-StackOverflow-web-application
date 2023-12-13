362492a977c3963926b2fc7c519e9e334b817b8b
ultramarine <2549952274@qq.com>
2023-12-13 12:11:57 +0800

    bug/topic查询接口测试正常（TODO:我颜色呢？）

--- frontend/src/components/bug/bugViewBar.vue
+++ frontend/src/components/bug/bugViewBar.vue
@@ -30,7 +30,7 @@
         axios.get(`/bug/topKByViewCount/${10}`, {}, {})
             .then(response => {
               items.value = response.data
-              init(JSON.stringify(items.value))
+              // init(JSON.stringify(items.value))
             })
             .catch(error => {
               if (error.response) {
