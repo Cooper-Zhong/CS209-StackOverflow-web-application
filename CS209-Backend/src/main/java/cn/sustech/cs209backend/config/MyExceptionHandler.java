3596668168dda5ac0748d716ebbcfc6b97ed2fc8
ultramarine <2549952274@qq.com>
2023-12-18 18:49:19 +0800

    添加log！！！！

--- CS209-Backend/src/main/java/cn/sustech/cs209backend/config/MyExceptionHandler.java
+++ CS209-Backend/src/main/java/cn/sustech/cs209backend/config/MyExceptionHandler.java
@@ -1,17 +1,20 @@
 package cn.sustech.cs209backend.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cn.sustech.cs209backend.dto.GlobalResponse;
 @ControllerAdvice
+@Slf4j
 public class MyExceptionHandler {
 
     @ExceptionHandler(value = MyException.class)
     @ResponseBody
     public GlobalResponse<String> exceptionHandler(MyException e){
-        System.out.println("User Defined Exception ！Reason is :"+e);
+//        System.out.println("User Defined Exception ！Reason is :"+e);
+        log.warn("User Defined Exception ！Reason is :"+e);
         return GlobalResponse.<String>builder()
             .code(e.getCode())
             .msg(e.getMessage())
@@ -21,7 +24,8 @@ public class MyExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public GlobalResponse<String> exceptionHandler(Exception e){
-        System.out.println("Unknow Exception！Reason is :"+e);
+//        System.out.println("Unknow Exception！Reason is :"+e);
+        log.warn("Unknow Exception！Reason is :"+e);
         return GlobalResponse.<String>builder()
             .code(-10086)
             .msg(e.getMessage() + e.getStackTrace())
