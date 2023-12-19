8d64df76708f211d729a723f8898770ee9642203
ultramarine <2549952274@qq.com>
2023-12-20 01:53:10 +0800

    改为使用饼图。。。。啊啊啊。。。。。

--- frontend/src/components/bugCompare/bugCompareBar.vue
+++ frontend/src/components/bugCompare/bugCompareBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="evaluationDimension" style="width: 100%; height: 270px"></div>
+    <div ref="evaluationDimension" style="width: 100%; height: 480px"></div>
   </template>
   
     
@@ -63,41 +63,139 @@
       };
   onMounted(() => {
     getCompare()
+    initDimension()
   });
-  
-  const initDimension = (exception, syntaxError, fatalError) => {
+  // exception, syntaxError, fatalError
+  const initDimension = () => {
     var myChart = echarts.init(evaluationDimension.value);
     var option;
   
+    // option = {
+    //   toolbox: {
+    //     show: true,
+    //   },
+    //   xAxis: {
+    //     data: ['Avarage Answer Count', 'Total Answer Count', 'Avarage View Count',
+    //      'Total View Count', 'Question Count', 'Avarage Score']
+    //   },
+    //   yAxis: {},
+    //   series: [
+    //     {
+    //       type: 'bar',
+    //       data: exception,
+    //       itemStyle: {
+    //         color: '#77CEFF' // 设置颜色
+    //       } 
+    //     },
+    //     {
+    //       type: 'bar',
+    //       data: fatalError,
+    //       itemStyle: {
+    //         color: '#0079AF' // 设置颜色
+    //       } 
+    //     },
+    //     {
+    //       type: 'bar',
+    //       data: syntaxError,
+    //       itemStyle: {
+    //         color: '#123E6B' // 设置颜色
+    //       } 
+    //     }
+    //   ]
+    // };
     option = {
-      xAxis: {
-        data: ['Avarage Answer Count', 'Total Answer Count', 'Avarage View Count',
-         'Total View Count', 'Question Count', 'Avarage Score']
-      },
-      yAxis: {},
-      series: [
-        {
-          type: 'bar',
-          data: exception,
-          itemStyle: {
-            color: '#77CEFF' // 设置颜色
-          } 
-        },
-        {
-          type: 'bar',
-          data: fatalError,
-          itemStyle: {
-            color: '#0079AF' // 设置颜色
-          } 
-        },
-        {
-          type: 'bar',
-          data: syntaxError,
-          itemStyle: {
-            color: '#123E6B' // 设置颜色
-          } 
-        }
+    legend: {},
+    tooltip: {},
+    dataset: {
+      source: [
+        ['bug', '1', '2', '3', '4', '5', '6'],
+        ['Milk Tea', 86.5, 92.1, 85.7, 83.1,],
+        ['Matcha Latte', 41.1, 30.4, 65.1, 53.3],
+        ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
+        ['Walnut Brownie', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
       ]
+    },
+    series: [
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['25%', '30%'],
+        encode: {
+          itemName: 'bug',
+          value: '1'
+        },
+        name: 'Average View Count',
+        label: {
+          show: false,
+        }
+        // No encode specified, by default, it is '2012'.
+      },
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['75%', '30%'],
+        encode: {
+          itemName: 'bug',
+          value: '2'
+        },
+        name: 'Average Answer Count',
+        label: {
+          show: false,
+        }
+      },
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['25%', '75%'],
+        encode: {
+          itemName: 'bug',
+          value: '3'
+        },
+        name: 'Total Answer Count',
+        label: {
+          show: false,
+        }
+      },
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['25%', '75%'],
+        encode: {
+          itemName: 'bug',
+          value: '4'
+        },
+        name: 'Total Question Count',
+        label: {
+          show: false,
+        }
+      },
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['75%', '75%'],
+        encode: {
+          itemName: 'bug',
+          value: '5'
+        },
+        name: 'Total Score',
+        label: {
+          show: false,
+        }
+      },
+      {
+        type: 'pie',
+        radius: '20%',
+        center: ['75%', '75%'],
+        encode: {
+          itemName: 'bug',
+          value: '6'
+        },
+        name: 'Average Score',
+        label: {
+          show: false,
+        }
+      },
+    ]
     };
     option && myChart.setOption(option);
   }
